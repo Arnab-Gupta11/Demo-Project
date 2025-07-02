8e574985e4d5e4a5c750ff2cc629e2fe38e99400
Arnab Gupta <arnabgupta1111@gmail.com>
2025-07-03 00:49:52 +0600

    Initialize Project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className='max-w-[1620px] mx-auto'>
+        <Header/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
